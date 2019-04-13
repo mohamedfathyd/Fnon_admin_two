fe6fac7021739c3d3b36395f255152ad12d8ec04
mohamed fathy <m.fathy7793@gmail.com>
2019-04-14 00:08:04 +0200

    cfsdf

--- app/src/main/java/epl/eldaf_electrony/fnon_admin/Model/ApiConfigration.java
+++ app/src/main/java/epl/eldaf_electrony/fnon_admin/Model/ApiConfigration.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiConfigration extends SyncStateContract.Constants {
-    private static final String url="http://192.168.1.10:8080/";
+    private static final String url="http://mano-ad.com/";
     private static Retrofit retrofit =null;
     public static Retrofit getapiClient(){
         if(retrofit== null){
--- app/src/main/java/epl/eldaf_electrony/fnon_admin/Model/Response_api.java
+++ app/src/main/java/epl/eldaf_electrony/fnon_admin/Model/Response_api.java
@@ -2,6 +2,8 @@ package epl.eldaf_electrony.fnon_admin.Model;
 
 import java.util.List;
 
+import epl.eldaf_electrony.fnon_admin.Model.contact_group;
+import epl.eldaf_electrony.fnon_admin.Model.contact_teacher;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -10,22 +12,22 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface Response_api {
-    @GET("Fnon_all_teacher.php")
+    @GET("fnon/Fnon_all_teacher.php")
     Call<List<contact_teacher>> getcontacts_teacher();
-    @GET("Fnon_all_group.php")
+    @GET("fnon/Fnon_all_group.php")
     Call<List<contact_group>> getcontacts_group();
     @FormUrlEncoded
-    @POST("Fnon_delete_group.php")
+    @POST("fnon/Fnon_delete_group.php")
     Call<ResponseBody> getcontacts_delete_group(@Field("id") int id);
     @FormUrlEncoded
-    @POST("Fnon_delete_teacher.php")
+    @POST("fnon/Fnon_delete_teacher.php")
     Call<ResponseBody> getcontacts_delete_teacher(@Field("id") int id);
     @FormUrlEncoded
-    @POST("Fnon_add_group.php")
+    @POST("fnon/Fnon_add_group.php")
     Call<ResponseBody> getcontacts_add_group(@Field("name") String name, @Field("teacher_id") int teacher_id, @Field("year") String year, @Field("group_name") String group_name
     , @Field("type") String type, @Field("Stage") String Stage, @Field("num_student") String num_student, @Field("price") double price);
     @FormUrlEncoded
-    @POST("Fnon_add_teacher.php")
+    @POST("fnon/Fnon_add_teacher.php")
     Call<ResponseBody> getcontacts_add_teacher(@Field("name") String name,@Field("phone") String phone,@Field("password") String password,@Field("Stage") String Stage
             ,@Field("address") String address);
 
